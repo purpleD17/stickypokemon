805719082a7909a8a8fac0f852a30e01e35de5ab
Glenn Barney <glennbarney@Glenns-MBP.home>
2016-04-01 00:08:46 -0400

    trying

--- start.js
+++ start.js
@@ -5,13 +5,15 @@ var server = require("http").createServer(app);
 var sticky = require("sticky-session");
 var cluster = require("cluster"); // Only required if you want the worker id
 var io = require ("socket.io").listen(server);
-var port = 8080;
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+
 
 app.get("/", function (req, res) {
     res.send("pokemon");
 });
 
-var startServer = sticky.listen(server, port);
+var startServer = sticky.listen(server, app.get('port'));
 
 if (!startServer) {
     // MASTER code here runs only in master process
